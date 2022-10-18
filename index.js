1922a41a16b13ce100e2d54fa33eeefd3fb5247b
Mikel Lasa <mikel.lasa@ikasle.aeg.eus>
2022-10-18 13:21:01 +0200

    working on firebase admin

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const route = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send(`Hello ${process.env.ROLE_JOSHUA}`);
+// import firebase-admin package
+const admin = require("firebase-admin");
+
+// import service account file (helps to know the firebase project details)
+const serviceAccount = require("./serviceAccountKey.json");
+
+// Intialize the firebase-admin project/account
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.use(cors());
+var jsonParser = bodyParser.json();
+
+app.get("/status", (req, res) => {
+  res.send("ckeck Status");
+});
+
+// Custom Verification Link
+app.post("/signup", jsonParser, async (req, res) => {
+  const userData = req.body;
+  console.log(userData);
+  const actionCodeSettings = {
+    url: `<hosted_firebase_url_link>`,
+    handleCodeInApp: true,
+    android: {
+      packageName: "<project_id>",
+    },
+  };
+  admin
+    .auth()
+    .generateSignInWithEmailLink(userData.email, actionCodeSettings)
+    .then(async (link) => {
+      // We got the Embedded Link (Now we can use this link and send the mail to the user
+      // for verifing the Email/account). To send custom mail you can use nodemailer or mail gun.
+    })
+    .catch((error) => {
+      res.json({
+        error: false,
+        message: error,
+      });
+    });
 });
