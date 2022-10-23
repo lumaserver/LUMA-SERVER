b30f75c562f5e4e99a0605d1b4cc364d956c832c
Mikel Lasa <2002mikel@gmail.com>
2022-10-23 15:47:56 +0200

    [UPDATE] develop finished

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express= require("express");
 const router = express.Router();
+
 const authMiddleware= require('../middleware/userMiddleware');
 const userController = require("../controllers/userController");
 
 router.post("/", authMiddleware, userController.createNewUser);
-//router.patch("/:userId", userController.updateOneUser);
 
 module.exports= router;
