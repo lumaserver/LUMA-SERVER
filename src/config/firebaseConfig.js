66c43db7c3a97b3a015e99326056d6b1eb556410
Mikel Lasa <mikel.lasa@ikasle.aeg.eus>
2022-11-09 10:04:02 +0100

    [UPDATE] PRIVATEKEY replace added

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,11 @@
 var admin = require("firebase-admin");
 
-
 admin.initializeApp({
   credential: admin.credential.cert({
-      projectId: process.env.PROJECT_ID,
-      privateKey: process.env.PRIVATE_KEY
-      ? process.env.PRIVATE_KEY.replace(/\\n/gm, "\n")
-      : undefined,
-      clientEmail: process.env.CLIENT_ID,
-  })
+    projectId: process.env.PROJECT_ID,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.CLIENT_ID,
+  }),
 });
 
-module.exports= admin;
+module.exports = admin;
