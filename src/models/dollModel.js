f7b23e89fc7d62e693e6c8a8bb12936e6362d36d
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2022-11-16 09:29:35 +0100

    [UPDATE] doll and dollPiece schema

--- src/models/dollModel.js
+++ src/models/dollModel.js
@@ -1,20 +1,10 @@
 const mongoose = require("mongoose");
+
 const { Schema } = mongoose;
 
 const dollSchema = new Schema({
-  isMissionFinished: Boolean,
-  isMissionPending: Boolean,
-  bodyPart: [
-    {
-      bodyName: String,
-      bodyData: {
-        isFound: Boolean,
-        xPosition: mongoose.Types.Decimal128,
-        yPosition: mongoose.Types.Decimal128,
-        image: String,
-      },
-    },
-  ],
+  missionStatus: { type: String, default: 'missionStarted' },
+  bodyPart: [{ type: Schema.ObjectId, ref: "DollPieceModel" }],
 });
 
 module.exports = mongoose.model("Doll", dollSchema);
