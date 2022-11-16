dae1cdd7e2c6fabe8af6ec654092abff81c2eb25
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2022-11-16 09:53:52 +0100

    [UPDATE] create dollPieces archive

---
+++ src/data/dollPieces.js
@@ -0,0 +1,18 @@
+[
+    {
+        "name": "head",
+        "img": "head.jpg"
+    },
+    {
+        "name": "body",
+        "img": "body.jpg"
+    },
+    {
+        "name": "arm",
+        "img": "arm.jpg"
+    },
+    {
+        "name": "leg",
+        "img": "leg.jpg"
+    }
+]
