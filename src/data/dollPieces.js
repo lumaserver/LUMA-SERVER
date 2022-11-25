610a575c99658e93e698f50c4ea04b72a6bb912b
Mikel Lasa <mikel.lasa@ikasle.aeg.eus>
2022-11-25 08:58:54 +0100

    [UPDATE] image url changed

--- src/data/dollPieces.js
+++ src/data/dollPieces.js
@@ -1,20 +1,20 @@
 const dollPieces = [
-    {
-        "pieceName": "head",
-        "image": "head.jpg"
-    },
-    {
-        "pieceName": "body",
-        "image": "body.jpg"
-    },
-    {
-        "pieceName": "arm",
-        "image": "arm.jpg"
-    },
-    {
-        "pieceName": "leg",
-        "image": "leg.jpg"
-    }
-]
+  {
+    pieceName: "head",
+    image: "https://i.ibb.co/XDY21R9/head.jpg",
+  },
+  {
+    pieceName: "body",
+    image: "https://i.ibb.co/jrZS68D/body.jpg",
+  },
+  {
+    pieceName: "arm",
+    image: "https://i.ibb.co/fqbH5jJ/arm.jpg",
+  },
+  {
+    pieceName: "leg",
+    image: "https://i.ibb.co/6nrZh9m/leg.jpg",
+  },
+];
 
 module.exports = dollPieces;
