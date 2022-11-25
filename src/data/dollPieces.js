a97904a871e94a36f2bd857343837816d11015cc
Mikel Lasa <mikel.lasa@ikasle.aeg.eus>
2022-11-25 08:56:20 +0100

    [UPDATE] image url changed

--- src/data/dollPieces.js
+++ src/data/dollPieces.js
@@ -1,20 +1,20 @@
 const dollPieces = [
-    {
-        "pieceName": "head",
-        "image": "require('../src/assets/head.jpg')"
-    },
-    {
-        "pieceName": "body",
-        "image": "require('../src/assets/body.jpg')"
-    },
-    {
-        "pieceName": "arm",
-        "image": "require('../src/assets/arm.jpg')"
-    },
-    {
-        "pieceName": "leg",
-        "image": "require('../src/assets/leg.jpg')"
-    }
-]
+  {
+    pieceName: "head",
+    image: "https://i.ibb.co/XDY21R9/head.jpg",
+  },
+  {
+    pieceName: "body",
+    image: "https://i.ibb.co/jrZS68D/body.jpg",
+  },
+  {
+    pieceName: "arm",
+    image: "https://i.ibb.co/fqbH5jJ/arm.jpg",
+  },
+  {
+    pieceName: "leg",
+    image: "https://i.ibb.co/6nrZh9m/leg.jpg",
+  },
+];
 
 module.exports = dollPieces;
