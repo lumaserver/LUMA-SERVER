7353a1e04fa6ac3e4ba71901122a94e4f6f1e192
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2022-12-21 16:33:12 +0100

    [UPDATE] modify getAllDollPieces

--- src/database/Doll.js
+++ src/database/Doll.js
@@ -42,7 +42,7 @@ const getAllDollPieces = async () => {
    //const allDollParts = await DollPiece.find();
    
 
-    return allDollParts[0];
+    return allDollParts;
   } catch (error) {
     console.log(error);
     throw error;
