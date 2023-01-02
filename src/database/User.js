1fd233a8c440ffd0d8495e061ff768765f479b00
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2023-01-02 17:23:48 +0100

    [UPDATE] console.log resistance

--- src/database/User.js
+++ src/database/User.js
@@ -98,10 +98,11 @@ const changeCryptValue = async (email) => {
 const updateUser = async (updateData) => {
   try {
   const filter = { email: updateData.email };
- 
-   if(updateData.resistance === POTION_RESISTANCE_VALUE ){
+ console.log(`Update Acolit ${updateData.resistance}`)
+   if(updateData.resistance == POTION_RESISTANCE_VALUE ){
     updateData.acolitStatus = "awake"
   } 
+
 
   const acolitUpdate = await User.findOneAndUpdate(filter, updateData, {
   new: true,
