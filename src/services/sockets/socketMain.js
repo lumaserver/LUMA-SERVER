58f2fd56691422d5968bdbfdb762f4ef3010ca31
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2023-01-26 12:39:33 +0100

    [UPDATE] console log req.headers

--- src/services/sockets/socketMain.js
+++ src/services/sockets/socketMain.js
@@ -7,4 +7,19 @@ io.on("connection", socketEvents);
 
 module.exports = io;
 
+/* 
+const jwt = require('jsonwebtoken');
 
+io.use(function(socket, next){
+  if (socket.handshake.query && socket.handshake.query.token){
+    jwt.verify(socket.handshake.query.token, 'SECRET_KEY', function(err, decoded) {
+      if (err) return next(new Error('Authentication error'));
+      socket.decoded = decoded;
+      next();
+    });
+  }
+  else {
+    next(new Error('Authentication error'));
+  }    
+})
+.on('connection', socketEvents); */
