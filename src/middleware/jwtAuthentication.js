cd14101913699a86c82c78508fced1170bbed303
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2023-01-26 12:56:08 +0100

    [UPDATE] console log req.headers

--- src/middleware/jwtAuthentication.js
+++ src/middleware/jwtAuthentication.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-console.log("aaa" + req.headers)
+console.log("aaa" + req.headers["authorization"])
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
