a86a4bd2f582147f0bd87ed80226a1e0afa3091c
Larraitz Arburua <larraitz.arburua@ikasle.aeg.eus>
2023-01-26 12:52:59 +0100

    [UPDATE] jwtMiddelware

--- src/routes/allDataRoute.js
+++ src/routes/allDataRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-//const jwtMiddleWare = require("../middleware/jwtAuthentication");
+const jwtMiddleWare = require("../middleware/jwtAuthentication");
 
 const dataController = require("../controllers/dataController");
 
-router.get("/:email", dataController.getAllData);
+router.get("/:email", jwtMiddleWare, dataController.getAllData);
 
 module.exports = router;
