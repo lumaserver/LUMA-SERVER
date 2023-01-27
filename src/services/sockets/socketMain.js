dcd153ab1e40b2d3899b8b31461a7b673e65096c
axarig <aritz.ibanez@ikasle.aeg.eus>
2023-01-27 10:54:08 +0100

    [UPDATE] io.use console.log changed

--- src/services/sockets/socketMain.js
+++ src/services/sockets/socketMain.js
@@ -5,7 +5,7 @@ const io = server.socketIO;
 const socketEvents = require("./socketEvents").socketEvents;
 
 io.use(function (socket, next) {
-    console.log(`middleware socket ${socket}`)
+    console.log(`middleware socket ${socket.handshake.query} && ${socket.handshake.query.token}`)
   if (socket.handshake.query && socket.handshake.query.token) {
     console.log(`If socket ${socket.handshake.query.token}`)
     jwt.verify(
