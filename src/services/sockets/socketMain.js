82ba5a5e4f2726f7ec95c4063959615594927d42
axarig <aritz.ibanez@ikasle.aeg.eus>
2023-01-27 10:37:26 +0100

    [UPDATE] Socket Authentication middleware added

--- src/services/sockets/socketMain.js
+++ src/services/sockets/socketMain.js
@@ -4,19 +4,22 @@ const io = server.socketIO;
 
 const socketEvents = require("./socketEvents").socketEvents;
 
-// io.use(function(socket, next){
-//   if (socket.handshake.query && socket.handshake.query.token){
-//     jwt.verify(socket.handshake.query.token, 'SECRET_KEY', function(err, decoded) {
-//       if (err) return next(new Error('Authentication error'));
-//       socket.decoded = decoded;
-//       next();
-//     });
-//   }
-//   else {
-//     next(new Error('Authentication error'));
-//   }    
-// }).
+io.use(function (socket, next) {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    jwt.verify(
+      socket.handshake.query.token,
+      "SECRET_KEY",
+      function (err, decoded) {
+        if (err) return next(new Error("Authentication error"));
+        socket.decoded = decoded;
+        next();
+      }
+    );
+  } else {
+    next(new Error("Authentication error"));
+  }
+}).on("connection", socketEvents);
 
-io.on("connection", socketEvents)
+//io.on("connection", socketEvents)
 
 module.exports = io;
